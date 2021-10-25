ce20d11becf064425d205e9a95c727c4e00b3b64
Your Name <you@example.com>
2021-10-25 16:26:12 +0700

    Pertemuan 2

---
+++ pertemuan3.py
@@ -0,0 +1,55 @@
+# name = input('Enter Name')
+# print('Your Name is'+ name)
+# my_string = 'Miaw World'
+# print(len(my_string))
+# print(my_string[0:6])
+# my_string = my_string.replace('World','Friends')
+# print(my_string)
+
+a = 'Miaw'
+b = 'World'
+c = a + ' ' + b
+print(c)
+
+a = "Nadiyah"
+b = 2121
+c = a + ' ' + str(b)
+print(c)
+
+nama = 'Nadi'
+perasaan = 'Happy'
+my_profile ='Nama saya {}, perasaan saya hari ini {}'. format (nama,perasaan)
+print(my_profile)
+
+x = 'Indonesia ai'
+print(x[2:4])
+
+#BOOLEANS
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+a = 10
+b = 9
+if b > a:
+    print('b is greater than a')
+else: 
+    print('b is less than a')
+
+#STRING
+my_list = []
+my_list.append('aku')
+my_list.append('bahagia')
+my_list.append(1)
+print(my_list)
+
+experience = 4
+level = 3
+if experience <= 3 and level <= 2:
+    print ('Gaji 9.000.000')
+elif experience >= 4 and level >= 3:
+    print ('Gaji 15.000.000')
+else:
+    print ('Gaji 20.000.000')
+
+
